cb66c700ad6ee5a820b10a155dbf76bbf921ef79
Chuck Heintzelman <chuckh@gmail.com>
2015-05-25 10:14:22 -0700

    Commit for Chapter 9

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,50 @@
 var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
-    mix
-        .less('admin.less', 'public/assets/css')
-        .phpUnit();
+    var dtPluginsDir = 'vendor/bower_components/datatables-plugins/';
+
+    // Copy jquery, bootstrap, and font awesome
+    mix.copy(
+        'vendor/bower_components/jquery/dist/jquery.js',
+        'resources/assets/js/jquery.js'
+    ).copy(
+        'vendor/bower_components/bootstrap/less',
+        'resources/assets/less/bootstrap'
+    ).copy(
+        'vendor/bower_components/bootstrap/dist/js/bootstrap.js',
+        'resources/assets/js/bootstrap.js'
+    ).copy(
+        'vendor/bower_components/bootstrap/dist/fonts',
+        'public/assets/fonts'
+    ).copy(
+        'vendor/bower_components/fontawesome/less',
+        'resources/assets/less/fontawesome'
+    ).copy(
+        'vendor/bower_components/fontawesome/fonts',
+        'public/assets/fonts'
+    );
+
+    // Copy datatables
+    mix.copy(
+        'vendor/bower_components/datatables/media/js/jquery.dataTables.js',
+        'resources/assets/js/dataTables.js'
+    ).copy(
+        dtPluginsDir + 'integration/bootstrap/3/dataTables.bootstrap.css',
+        'resources/assets/less/dataTables.less'
+    ).copy(
+        dtPluginsDir + 'integration/bootstrap/3/dataTables.bootstrap.js',
+        'resources/assets/js/dataTables.bootstrap.js'
+    );
+
+    // Combine scripts
+    mix.scripts([
+        'js/jquery.js',
+        'js/bootstrap.js',
+        'js/dataTables.js',
+        'js/dataTables.bootstrap.js'
+
+    ], 'public/assets/js/admin.js', 'resources/assets');
+
+    // Compile Less
+    mix.less('admin.less', 'public/assets/css');
 });
--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -10,16 +10,15 @@
 
   @yield('styles')
 
-  <link href='//fonts.googleapis.com/css?family=Roboto:400,300'
-        rel='stylesheet' type='text/css'>
-
   <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js" \
-    ></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"> \
+  </script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"> \
+  </script>
   <![endif]-->
 </head>
 <body>
+
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header">
@@ -43,8 +42,7 @@
 @yield('content')
 
 <!-- Scripts -->
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+<script src="{{ asset('/assets/js/admin.js') }}"></script>
 
 @yield('scripts')
 
