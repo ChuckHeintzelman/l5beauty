e6f26d9fb18c6381845d26cd69aad163b5678b51
Chuck Heintzelman <chuckh@gmail.com>
2015-05-25 09:31:12 -0700

    Chapter 6 commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+## Laravel 5 Beauty
+
+A simple, clean and beautiful blogging application written in Laravel 5.
+
+This repository contains the source code to accompany the book
+[Laravel 5 Beauty](http://laravelcoding.com/blog?tag=L5+Beauty).
+
+This is the **Chapter 6** branch.
+
---
+++ tests/MarkdownerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+class MarkdownerTest extends TestCase
+{
+
+    protected $markdown;
+
+    public function setup()
+    {
+        $this->markdown = new \App\Services\Markdowner();
+    }
+
+    /**
+     * @dataProvider conversionsProvider
+     */
+    public function testConversions($value, $expected)
+    {
+        $this->assertEquals($expected, $this->markdown->toHTML($value));
+    }
+
+    public function conversionsProvider()
+    {
+        return [
+            ["test", "<p>test</p>\n"],
+            ["# title", "<h1>title</h1>\n"],
+            ["Here's Johnny!", "<p>Here&#8217;s Johnny!</p>\n"],
+        ];
+    }
+}
